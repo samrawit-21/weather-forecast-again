4f6d593b210167d29477adb413be53f08e501494
samrawit-21 <samrawityisak661@gmail.com>
2026-01-09 15:02:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function searchCity(city) {
-let apiKey = "o0334a459at0ffc9a191a1b183f3f306";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-console.log(apiUrl);
+function refreshWeather(response) {
+     console.log(response);
 }
+function searchCity(city) {
+    let apiKey = "o0334a459at0ffc9a191a1b183f3f306";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiUrl}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 
 function followLink(event) {
     event.preventDefault();
     let inputElement = document.querySelector("#search-input");
     let headingElement = document.querySelector("#weather-city");
     headingElement.innerHTML = inputElement.value;
+    searchCity(inputElement.value);
 }
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",followLink);
